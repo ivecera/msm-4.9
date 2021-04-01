b73a7376e6b753d99b9e0331781cf94c0fa36ba0
Ivan Vecera <ivan@cera.cz>
2021-04-01 14:03:32 +0200

    LEDS
    Change-Id: I017686143a7e1bce5c2a3411d6e1693187c6a815

--- drivers/leds/leds-qpnp-haptics.c
+++ drivers/leds/leds-qpnp-haptics.c
@@ -521,7 +521,7 @@ static int qpnp_haptics_auto_res_enable(struct hap_chip *chip, bool enable)
 	if (auto_res_mode_qwd && enable)
 		usleep_range(delay_us, delay_us + 1);
 
-	if (socinfo_get_platform_type() == QCOM_HW_PLATFORM_GRUS)
+	if (socinfo_get_platform_type() == QCOM_HW_PLATFORM_GRUS) {
 		pr_debug("hardcode val=0 upon Qualcomm change solution in HARDWARE_PLATFORM_GRUS;\n");
 		enableval = 0;
 	}
